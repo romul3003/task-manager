22387d6d615ab285c463f7cc0e967dfbbb1efa86
Roman Zakrevskyi <romul3003@gmail.com>
2022-09-15 16:58:47 +0300

    Add control for creating a new task

--- src/components/FormControls.tsx
+++ src/components/FormControls.tsx
@@ -1,15 +1,27 @@
 import { FC } from 'react'
+import { useDispatch } from 'react-redux'
 import { Button, SxProps, Theme } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add'
+import { setTaskManagerState } from '../redux/tasks/actions'
+import { FormStates } from '../types'
 
-const FormControls: FC<{sx?: SxProps<Theme>}> = ({ sx }) => (
-  <Button
-    startIcon={<AddIcon />}
-    variant="text"
-    sx={sx}
-  >
-    New Task
-  </Button>
-)
+const FormControls: FC<{sx?: SxProps<Theme>}> = ({ sx }) => {
+  const dispatch = useDispatch()
+
+  const setCreateTaskFormState = () => {
+    dispatch(setTaskManagerState(FormStates.CREATE))
+  }
+
+  return (
+    <Button
+      startIcon={<AddIcon />}
+      variant="text"
+      sx={sx}
+      onClick={setCreateTaskFormState}
+    >
+      New Task
+    </Button>
+  )
+}
 
 export default FormControls
